add76dfebe8c2b7d11676e50e9d6e1f147adf11b
vinayakck2002 <vinayakck317@gmail.com>
2026-02-21 09:52:20 +0530

    blacklist removed

--- restaurant_backend/settings.py
+++ restaurant_backend/settings.py
@@ -143,9 +143,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),  
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,    
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),   
 }
 
 
