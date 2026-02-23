ac19e31b5fc1e0bd954ed8137932d867ada76810
Prajinprakash2025 <prajinprakash722@gmail.com>
2026-02-23 21:24:14 +0530

    Fix decimal default value in migration file

--- inventory/views.py
+++ inventory/views.py
@@ -11,7 +11,7 @@ from .serializers import CategorySerializer, MenuItemSerializer
 # CUSTOM PAGINATION
 # ==========================================
 class AdminPagination(PageNumberPagination):
-    page_size = 10 
+    page_size = 12
     page_size_query_param = 'page_size'
     max_page_size = 100
 
