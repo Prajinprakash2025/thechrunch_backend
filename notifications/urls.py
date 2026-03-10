eb842d2aa879d1575f01c3832b8f0d3fccc093d2
vinayakck2002 <vinayakck317@gmail.com>
2026-03-10 23:33:38 +0530

    delete added for logout

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,6 +1,8 @@
+# notifications/urls.py
 from django.urls import path
-from .views import SaveFCMTokenView
+from .views import SaveFCMTokenView, DeleteFCMTokenView # Randum import cheyyanam
 
 urlpatterns = [
     path('save-fcm-token/', SaveFCMTokenView.as_view(), name='save_fcm_token'),
+    path('delete-fcm-token/', DeleteFCMTokenView.as_view(), name='delete_fcm_token'), # Ithu add cheyyuka
 ]
