c63f1f54bdb6ad94a9f402c02e6a307fc9f9b68e
vinayakck2002 <vinayakck317@gmail.com>
2026-03-10 16:19:23 +0530

    function added ordercount

--- customers/serializers.py
+++ customers/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from .models import CustomUser
+from orders.models import Order 
 
 User = get_user_model()
 
@@ -8,3 +10,7 @@ class CustomerSerializer(serializers.ModelSerializer):
         model = User
         # NOTE: Make sure 'phone_number' matches your exact field name in the User model
         fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'is_blocked', 'date_joined','total_orders']
+        # Ee function ezhuthuka
+    def get_total_orders(self, obj):
+        # Oru user-de total completed orders count edukkan
+        return Order.objects.filter(user=obj).count()
