6162db460d6256b1f89e79666fcfd99574c601cc
vinayakck2002 <vinayakck317@gmail.com>
2026-04-17 22:15:18 +0530

    authentication

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,16 +1,9 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
 class CookieJWTAuthentication(JWTAuthentication):
-    """
-    Custom Authentication Class.
-    Tells Django REST Framework to look for the JWT token inside the HTTP-Only cookie.
-    """
     def authenticate(self, request):
-        # Aadyam standard Header-il token undo ennu nokkum
         header = self.get_header(request)
         
-        # Header-il illengil (Cookie aayathukondu undavilla), Cookie-il ninnu edukkum
         if header is None:
+            # Header ഇല്ലെങ്കിൽ കുക്കിയിൽ നിന്ന് എടുക്കാൻ നോക്കുന്നു
             raw_token = request.COOKIES.get('access_token')
         else:
             raw_token = self.get_raw_token(header)
@@ -18,6 +11,12 @@ class CookieJWTAuthentication(JWTAuthentication):
         if raw_token is None:
             return None
 
-        # Token Validate cheythu User-e return cheyyunnu
-        validated_token = self.get_validated_token(raw_token)
-        return self.get_user(validated_token), validated_token
+        try:
+            # ടോക്കൺ വാലിഡേറ്റ് ചെയ്യുന്നു
+            validated_token = self.get_validated_token(raw_token)
+            return self.get_user(validated_token), validated_token
+        except:
+            # 🌟 ഇതാണ് മെയിൻ മാറ്റം:
+            # ടോക്കൺ എക്സ്പെയർ ആയാലോ ഇൻവാലിഡ് ആയാലോ എറർ കാണിക്കാതെ 
+            # വെറുതെ None തിരിച്ചു വിടുക. അപ്പോൾ സിസ്റ്റം ലോഗിൻ ചെയ്യാൻ അനുവദിക്കും.
+            return None
